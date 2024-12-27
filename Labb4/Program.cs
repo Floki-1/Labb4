0d77f348f21fc80a0ff8a62fc189f8601ae04739
Floki-1 <mobeenjamil.1990@gmail.com>
2024-12-27 21:04:23 +0100

    Add files via upload

---
+++ Labb4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO.Pipes;
+
+namespace Labb4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Person person = new Person();
+            person.Name = "Test";
+            person.BirthDate = DateTime.Now;
+
+         
+               
+           
+
+
+            
+
+
+
+
+        }
+        
+
+       
+
+
+       
+
+                
+
+
+
+        
+    }
+}
